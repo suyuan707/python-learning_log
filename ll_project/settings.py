1ec9fda5978c1be2bfea85617092a2910c6f5276
suyuan707 <1683361582@qq.com>
2026-05-03 23:01:27 +0800

    feat: 创建用户账号至登录页面

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # 我的应用程序
     'learning_logs',
+    'accounts',
 
     # Django 默认添加的应用程序
     'django.contrib.admin',
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# 我的设置
+LOGIN_REDIRECT_URL = 'learning_logs:index'
